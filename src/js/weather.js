8d9cb387b9b77301fa068fada5541555e08c2c52
kimjisu1106 <kimjisu1106@gmail.com>
2022-12-31 01:30:17 +0900

    create weather.js

---
+++ src/js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "79365b1d2caff9a079b551800f8530e5";
+
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main;
+    });
+}
+
+function onGeoError() {}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
